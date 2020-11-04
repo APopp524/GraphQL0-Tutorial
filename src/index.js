99761dec6bfe232f2d24b77a19b7724b492f3748
Andrew Popp <apopp524@gmail.com>
2020-11-04 10:19:54 -0600

    added apollo express and graphQL scheme and resolvers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-import 'dotenv/config';
- 
-import saySomething from './my-other-file.js'
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+
+const app = express();
+
+const schema = GQ1`
+    type Query {
+        me: User
+    }
+        type User {
+            username: String!
+        }
+    `;
+
+const resolvers = {
+    Query: {
+        me: () => {
+            return {
+                username: "Andrew Popp",
+            };
+        },
+    },
+};
+
+const data = {
+    me: {
+        username: "Andrew Popp",
+    },
+};
+
+const server = new ApolloServer({
+    typeDefs: schema,
+    resolvers,
+});
+
+server.applyMiddleware({ app, path: '/graphq1' });
+
+app.listen({ port:8000 }, () => {
+    console.log('Apollo Server on https://localhost:8000/graphq1')
+});
